520feb43489f08867a0a4e035b987a117b556f13
Biplov Gautam <156338211+biplov2061@users.noreply.github.com>
2024-06-09 15:45:55 +0545

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,12 @@
+let questions = document.querySelectorAll(".Net_questions");
+
+questions.forEach(question =>{
+    const answer = question.querySelector(".answer");
+    question.addEventListener("click" , ()=>
+    {
+       answer.classList.toggle("active");
+    });
+
+
+});
+
